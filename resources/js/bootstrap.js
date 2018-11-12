f7eede2f57b0168e210137d012d896b69f9e75ea
Sun, Changming <changming.sun@accenture.com>
2018-11-12 16:22:42 +0800

    init commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,6 @@
 
 window._ = require('lodash');
+window.Popper = require('popper.js').default;
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -8,9 +9,8 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
-
+    require('datatables.net-dt');
     require('bootstrap');
 } catch (e) {}
 
