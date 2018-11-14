69cddf3593d8a3aa1e5d0263e34aae3b97ef2a9d
Sun, Changming <changming.sun@accenture.com>
2018-11-14 11:54:59 +0800

    commit

--- resources/views/timereport/index.blade.php
+++ resources/views/timereport/index.blade.php
@@ -7,11 +7,33 @@
 <script type="text/javascript">
 
 var dataSet = [
-    {"date" : "2018/11/7", "checkinTime" : "8:30","checkoutTime" : "13:30", "workTime" : "5:00", "emp_id" : "20999991", "name" : "小山　一昭" },
-    {"date" : "2018/11/8", "checkinTime" : "10:00","checkoutTime" : "21:00", "workTime" : "11:00", "emp_id" : "20999991", "name" : "小山　一昭" },
-    {"date" : "2018/11/9", "checkinTime" : "13:00","checkoutTime" : "(24:00)", "workTime" : "11:00", "emp_id" : "20999991", "name" : "小山　一昭" },
-    {"date" : "2018/11/10", "checkinTime" : "(00:00)","checkoutTime" : "12:00", "workTime" : "12:00", "emp_id" : "20999991", "name" : "小山　一昭" },
-    {"date" : "2018/11/11", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20999991", "name" : "小山　一昭" },
+  {"date" : "2018/11/8", "checkinTime" : "10:00","checkoutTime" : "21:00", "workTime" : "11:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/9", "checkinTime" : "13:00","checkoutTime" : "(24:00)", "workTime" : "11:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/10", "checkinTime" : "(00:00)","checkoutTime" : "12:00", "workTime" : "12:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/11", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/12", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/13", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/14", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/15", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/16", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/17", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/18", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/19", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/20", "checkinTime" : "8:30","checkoutTime" : "18:30", "workTime" : "10:00", "emp_id" : "20973323", "name" : "野比　大雄" },
+{"date" : "2018/11/7", "checkinTime" : "11:00","checkoutTime" : "21:00", "workTime" : "10:00", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/8", "checkinTime" : "11:10","checkoutTime" : "21:05", "workTime" : "9:55", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/9", "checkinTime" : "11:20","checkoutTime" : "21:10", "workTime" : "9:50", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/10", "checkinTime" : "11:30","checkoutTime" : "21:15", "workTime" : "9:45", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/11", "checkinTime" : "11:40","checkoutTime" : "21:20", "workTime" : "9:40", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/12", "checkinTime" : "11:50","checkoutTime" : "21:25", "workTime" : "9:35", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/13", "checkinTime" : "12:00","checkoutTime" : "21:30", "workTime" : "9:30", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/14", "checkinTime" : "12:10","checkoutTime" : "21:35", "workTime" : "9:25", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/15", "checkinTime" : "12:20","checkoutTime" : "21:40", "workTime" : "9:20", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/16", "checkinTime" : "12:30","checkoutTime" : "21:45", "workTime" : "9:15", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/17", "checkinTime" : "12:40","checkoutTime" : "21:50", "workTime" : "9:10", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/18", "checkinTime" : "12:50","checkoutTime" : "21:55", "workTime" : "9:05", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/19", "checkinTime" : "13:00","checkoutTime" : "22:00", "workTime" : "9:00", "emp_id" : "34212346", "name" : "刚田　武" },
+{"date" : "2018/11/20", "checkinTime" : "13:10","checkoutTime" : "22:05", "workTime" : "8:55", "emp_id" : "34212346", "name" : "刚田　武" },
 ];
 
 
@@ -19,7 +41,8 @@ $(document).ready(function() {
     var tblDatas = $('#example').DataTable( {
       ordering: false,
       info: false,
-      paging: false,
+      paging: true,
+      pageLength: 25,
       searching: false,
         data: dataSet,
         columns: [ 
@@ -69,7 +92,7 @@ $(document).ready(function() {
     <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group mr-2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <label for="inputdate" class="d-block w-100">日付</label>
-            <input type="text" id="inputdate" class="custom-control d-block w-100">
+            <input type="text" id="inputdate" class="custom-control d-block w-100" value="2018/11">
             &nbsp;&nbsp;&nbsp;&nbsp;<label for="inputname" class="d-block w-100">氏名</label>
             <input type="text" id="inputname" class="custom-control d-block w-100">
             &nbsp;&nbsp;&nbsp;&nbsp;<label for="inputid" class="d-block w-100">ID</label>
@@ -83,7 +106,7 @@ $(document).ready(function() {
           </div>
           <br>
           <div class="btn-group mr-2">
-            <label style="color:black;font-weight:bold">現行検索条件: </label>&nbsp;&nbsp;&nbsp;&nbsp;<label id="" style="color:black;font-weight:bold">ALL</label>
+            <label style="color:black;font-weight:bold">現行検索条件: </label>&nbsp;&nbsp;&nbsp;&nbsp;<label id="" style="color:black;font-weight:bold">日付: 当月 (2018/11)</label>
           </div>
     <br>
     <div class="table-responsive">
